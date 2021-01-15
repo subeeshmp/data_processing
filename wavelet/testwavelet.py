4d3de5ed5084ec7831c3f47092861835509b3810
p-amol <prakashamol@gmail.com>
2021-01-15 19:30:21 +0530

    Added path to test file

--- wavelet/testwavelet.py
+++ wavelet/testwavelet.py
@@ -11,9 +11,18 @@ Created on Fri Jan 15 19:05:05 2021
 """
 
 import numpy as np
+import sys
+import os
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
 import data_processing as dp
 
-sst = np.loadtxt('wavelet/sst_nino3.dat')  # input SST time series
+
+
+sst = np.loadtxt('sst_nino3.dat')  # input SST time series
 sst = sst - np.mean(sst)
 
 
